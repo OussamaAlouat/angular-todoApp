75339224f5d044f1381ead0ab8e9c5da204666e5
OussamaAlouat <al317257@uji.es>
2019-09-02 22:59:27 +0200

    Change activity properties

--- src/app/components/todo.component.ts
+++ src/app/components/todo.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import { faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { v4 as uuid } from 'uuid';
+
+import { Activity } from '../models/activity';
 @Component({
   selector: 'todo',
   templateUrl: '../views/todo.component.html'
@@ -6,21 +10,28 @@ import {Component} from '@angular/core';
 
 export class TodoComponent {
   public title: string;
-  public activities: Array<any>;
-  public activity: string;
+  public activities: Array<Activity>;
+  public activity: Activity;
+  public faTrash;
+  public faCheck;
+  public name: string;
 
   constructor() {
     this.title = 'TODO APP';
     this.activities = [];
-    this.activity = '';
+    this.activity = new Activity('', '', new Date(), false);
+    this.faCheck = faCheck;
+    this.faTrash = faTrash;
+    this.name = '';
+    console.log(this.activity)
   }
 
   public addActivity() {
-    const current = {
-      date: new Date(),
-      name: this.activity
-    }
-    this.activities.push(current);
-    this.activity = '';
+    this.activity.id = uuid();
+    this.activity.date = new Date();
+    this.activity.completed = false;
+    this.activity.name = this.name;
+    this.activities.push(this.activity);
+    this.name = '';
   }
 }
