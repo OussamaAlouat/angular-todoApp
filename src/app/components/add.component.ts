4cccee80e8e04b00d857c0d2ebf9bff30d4fd633
OussamaAlouat <al317257@uji.es>
2019-09-05 11:24:53 +0200

    Added new component to add activities

---
+++ src/app/components/add.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Activity } from '../models/activity';
+import { v4 as uuid } from 'uuid';
+import { TodoService } from '../services/todo.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'add-component',
+  templateUrl: '../views/add.component.html',
+  providers: [TodoService]
+})
+export class AddComponent {
+  public activity: Activity;
+  constructor(
+    private _todoService: TodoService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
+    this.activity = new Activity ('','', '',new Date(), false);
+  }
+
+  addActivity() {
+    this.activity.id = uuid();
+    this._todoService.addNewActivity(this.activity);
+    this._router.navigate(['/todo-list'])
+  }
+}
